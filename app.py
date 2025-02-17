8dd5c8c6b887113f32fc943fd2931db1cfbfc17a
Wellington Horn <wellingtondeveloper31@gmail.com>
2025-02-17 16:02:21 -0300

    update 2 17-02-2025

--- app.py
+++ app.py
@@ -41,4 +41,4 @@ def status():
     return jsonify({"status": "API is running"}), 200
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    app.run(debug=True, host='0.0.0.0', port=8000)
